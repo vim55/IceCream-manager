5a05a141a226773d24db1195871b6ac7ade3fcc1
Alvin Lin <alvin_00x@hotmail.com>
2023-03-17 02:30:54 +0800

    6 add html view with linking - not doen fully

--- IceCreamApp/views.py
+++ IceCreamApp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+from .models import *
 
 # Create your views here.
 def homePage(request):
-    return HttpResponse("Hello, World!")
+    #return HttpResponse("Hello, World!")
+    return render(request, "IceCreamApp/homePage.html") 
 
